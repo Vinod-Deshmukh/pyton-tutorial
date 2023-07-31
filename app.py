b8e086f02ee4520de4b4eb737c1c76b0903a9846
Vinod-Deshmukh <vinod.256.14@gmail.com>
2023-07-31 17:27:10 +0530

    type conversion

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-print("Vinod Deshmukh")
-print('O----')
-print(' ||||')
-print('*'*10)
+weight_pound=input('What is your weight in pound? ')
+weight_kilogram=int(weight_pound)*0.453592
+print(weight_kilogram)
