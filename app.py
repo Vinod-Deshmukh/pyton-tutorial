5db9e35323675d14fdc1178e2307fe6abcee4c6c
Vinod-Deshmukh <104995032+Vinod-Deshmukh@users.noreply.github.com>
2023-08-02 13:34:08 +0530

    comparison operators

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
-print("Hello World")
+print("Comparison Operator Tutorial")
+name="Python"
+if len(name)<3:
+    print("Name must be at least 3 characters")
+elif len(name)>50:
+    print("Name can be a maximum of 50 characters")
+else:print("Name looks good!")
