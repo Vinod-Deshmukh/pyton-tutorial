f2e494caa6ab9c55ef1b4e7e1d37aca25b01929b
Vinod-Deshmukh <104995032+Vinod-Deshmukh@users.noreply.github.com>
2023-08-02 15:40:19 +0530

    Weight converter program

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 print("Weight Converter Program")
+weight=float(input("Weight: "))
+unit=input("(L)bs or (K)g: ")
+if unit=="l" or unit=="L":
+    weight=weight*0.45
+    print(f'You are {weight} Kgs')
+else:
+    weight=weight/0.45
+    print(f'You are {weight} pounds')
+
