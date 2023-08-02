8994f5d7b727a1a8b88da89014b0cd5463e7045d
Vinod-Deshmukh <vinod.256.14@gmail.com>
2023-08-02 12:56:29 +0530

    string methods

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-print("Vinod Deshmukh")
-print('O----')
-print(' ||||')
-print('*'*10)
+name='Python'
+print(len(name))
+print(name.upper())
+print(name.lower())
+print(name.title())
+print(name.find('y'))
+print('p' in name)
+print('P' in name)
