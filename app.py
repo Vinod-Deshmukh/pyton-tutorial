33c60ff5abcadea02e8a0eef6c7915025a60ea6e
Vinod-Deshmukh <vinod.256.14@gmail.com>
2023-08-02 18:08:50 +0530

    while else gusseing game

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 print("#21 Building a Guessing Game!")
+number=10
+guess_count=1
+guess_limit=3
+while guess_count<=guess_limit:
+    guess=int(input("Guess: "))
+    guess_count=guess_count+1
+    if guess==number:
+        print("You Win!")
+        break
+
+else:print("Sorry, You Guessed wrong number.")
