bb351759cff282a61f96b6f40b93a090d68190eb
Vinod-Deshmukh <vinod.256.14@gmail.com>
2023-10-12 13:19:07 +0530

    while else car-game

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 print("#21 Car Game!")
 command = ""
-
+started=False
 while True:
     command = input(">").lower()
     if command == 'help':
          print('start- to start the car \nstop- to stop the car \nquit-to exit')
     elif command == 'start':
-        print('Car started.... Ready to go!')
+        if started:
+            print("car is already started.")
+        else:
+            started = True
+            print('Car started.')
     elif command == 'stop':
-        print('Car stopped.')
+        if not started:
+            print("car is already stopped")
+        else:
+            started=False
+            print('Car stopped.')
     elif command == 'quit':
         break
     else:
