6cbe9e931a8ce9724fe4387271c9fa3dc5f41270
Vinod-Deshmukh <vinod.256.14@gmail.com>
2023-10-13 10:28:57 +0530

    Lists

--- app.py
+++ app.py
@@ -1,7 +1,1 @@
-print("#25 Lists")
-numbers=[10,20,3000,141,2400,6000,8700]
-largest_number=0;
-for number in numbers:
-    if largest_number<number:
-        largest_number=number;
-print(largest_number)
+print("#26 2D Lists")
