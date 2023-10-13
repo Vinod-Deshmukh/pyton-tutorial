a5b944efa89e031f185c974399bd409bf0604d2a
Vinod-Deshmukh <vinod.256.14@gmail.com>
2023-10-13 10:44:07 +0530

    Lists methods

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("#27 Lists Methods")
+print("#27 Lists Methods: Write a Program to remove the duplicates on a List")
+numbers=[10,21,23,21,10,10,5,44]
+uniques=[]
+for number in numbers:
+    if number not in uniques:
+        uniques.append(number)
+print(numbers)
+print(uniques)
