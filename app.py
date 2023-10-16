cf958787db8fc857865a5a4952f24f9de4e55dc3
Vinod-Deshmukh <vinod.256.14@gmail.com>
2023-10-16 18:20:18 +0530

    Functions

--- app.py
+++ app.py
@@ -1,20 +1,7 @@
-print("#30 Dictionaries: Write a program which returns phone number into words")
-ph_number={
-    "0":"Zero",
-    "1":"One",
-    "2":"Two",
-    "3":"Three",
-    "4":"four",
-    "5":"five",
-    "6":"Six",
-    "7":"Seven",
-    "8":"Eight",
-    "9":"Nine"
-}
-numbers=input("Phone:")
-output=""
-for number in numbers:
-    output+=ph_number.get(number,"!")+ " "
-print(output)
+print("#32 Functions")
+def greet_user():
+    print("Welcome User")
+    print("May I Help you!")
 
 
+greet_user()
