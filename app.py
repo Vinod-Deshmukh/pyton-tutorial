60048a6fcaee1509ec047e1f5bfe2fec5cf0bb16
Vinod-Deshmukh <vinod.256.14@gmail.com>
2023-10-17 12:47:33 +0530

    keyword arguments

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 
-print("#33 Parameters")
+print("#34 Keyword Arguments")
 def greet_user(first_name,last_name):
     print(f"Welcome {first_name} {last_name} !")
-    print("May I Help you!")
+    # print("May I Help you!")
 
 
 greet_user("Vinod","Deshmukh")
+greet_user("Deshmukh","Vinod")
+greet_user(last_name="Deshmukh",first_name="Vinod")
+greet_user("Vinod",last_name="Deshmukh")
 
